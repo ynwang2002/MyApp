19cc49a3411c271b7966a68ea854cafa1692fa80
Yani <s1090350@gm.pu.edu.tw>
2022-05-26 01:04:43 +0800

    v.2

--- app/src/main/java/tw/edu/pu/s1090350/myapp/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1090350/myapp/MainActivity.kt
@@ -1,15 +1,48 @@
 package tw.edu.pu.s1090350.myapp
 
 import android.content.pm.ActivityInfo
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.coroutines.*
+import tw.edu.pu.s1090350.myapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img: ImageView
+    lateinit var mysv:MySurfaceView
+    lateinit var binding: ActivityMainBinding
+    var flag:Boolean = false
+    lateinit var job: Job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.img.setOnClickListener({
+            if (flag){
+                flag = false
+                binding.img.setImageResource(R.drawable.start)
+                job.cancel()
+            }
+            else{
+                flag = true
+                binding.img.setImageResource(R.drawable.stop)
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(flag) {
+                        delay(1)
+
+                        var canvas: Canvas = binding.mysv.surfaceHolder.lockCanvas()
+                        binding.mysv.drawSomething(canvas)
+                        binding.mysv.surfaceHolder.unlockCanvasAndPost(canvas)
+                    }
+                }
+
+            }
+        })
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
-
     }
 }
--- app/src/main/java/tw/edu/pu/s1090350/myapp/MySurfaceView.kt
+++ app/src/main/java/tw/edu/pu/s1090350/myapp/MySurfaceView.kt
@@ -5,47 +5,51 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-
 class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
     SurfaceHolder.Callback {
     var surfaceHolder: SurfaceHolder
     var BG: Bitmap
+    var BGmoveX:Int = 0
 
     init {
         surfaceHolder = getHolder()
         BG = BitmapFactory.decodeResource(getResources(), R.drawable.background)
         surfaceHolder.addCallback(this)
     }
-
     override fun surfaceCreated(p0: SurfaceHolder) {
-
-
         var canvas: Canvas = surfaceHolder.lockCanvas()
         drawSomething(canvas)
         surfaceHolder.unlockCanvasAndPost(canvas)
-
     }
 
     override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-
-
     }
-
     override fun surfaceDestroyed(p0: SurfaceHolder) {
-
     }
 
     fun drawSomething(canvas:Canvas) {
-        var SrcRect: Rect = Rect(0, 0, BG.width, BG.height) //裁切
+        var SrcRect: Rect = Rect(0, 0, BG.width, BG.height)
         var w:Int = width
         var h:Int = height
-        var DestRect:Rect = Rect(0, 0, w, h)
-        canvas.drawBitmap(BG, SrcRect, DestRect, null)
+
+        BGmoveX --
+        var BGnewX:Int = w + BGmoveX
+
+        if (BGnewX <= 0) {
+            BGmoveX = 0
+
+            canvas.drawBitmap(BG, BGmoveX.toFloat(), 0f, null)
+        } else {
+
+            var DestRect:Rect = Rect(BGmoveX, 0, BGmoveX+w, h)
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+            DestRect = Rect(BGnewX, 0, BGnewX+w, h)
+            canvas.drawBitmap(BG, SrcRect, DestRect, null)
+        }
 
         val paint = Paint(Paint.ANTI_ALIAS_FLAG)
         paint.color = Color.BLUE
         paint.textSize = 50f
         canvas.drawText("射擊遊戲(作者 : 王雅妮)", 50f,50f, paint)
-
     }
 }
