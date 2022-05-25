917a83f90dd4d9a098b7e3ab49d9776ce079c4db
Yani <s1090350@gm.pu.edu.tw>
2022-05-26 01:12:22 +0800

    v.3

--- app/src/main/java/tw/edu/pu/s1090350/myapp/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1090350/myapp/MainActivity.kt
@@ -5,8 +5,12 @@ import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
 import tw.edu.pu.s1090350.myapp.databinding.ActivityMainBinding
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +19,18 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     var flag:Boolean = false
     lateinit var job: Job
+    lateinit var handsomechang : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        GlideApp.with(this)
+            .load(R.drawable.me)
+            .circleCrop()
+            .override(800, 600)
+            .into(binding.ni)
 
         binding.img.setOnClickListener({
             if (flag){
